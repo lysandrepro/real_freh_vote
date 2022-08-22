4b2b9aa73676a344d4b99083c58e13f47ae34f44
clooney lysandre <clooney.lysandre@gmail.com>
2022-08-23 00:21:39 +0100

    this is the first commit shees

---
+++ fresvote_project/src/main/java/com/milol/fresvote_project/FresvoteProjectApplication.java
@@ -0,0 +1,13 @@
+package com.milol.fresvote_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FresvoteProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FresvoteProjectApplication.class, args);
+	}
+
+}
